cf5adeff85219edee5e5934765d2a5e56a9ef515
Rahul Singh <rahulsingh594@deloitte.com>
2024-09-18 21:18:04 +0530

    add update sqllite3 update version config file

--- projectResume/settings.py
+++ projectResume/settings.py
@@ -82,9 +82,8 @@ WSGI_APPLICATION = 'projectResume.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.dummy',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
