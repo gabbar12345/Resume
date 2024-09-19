9d43ad011c254386c0c43227be7d891b5bcb03e6
Rahul Singh <rahulsingh594@deloitte.com>
2024-09-19 12:36:21 +0530

    add allowed host

--- projectResume/settings.py
+++ projectResume/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-kr_1neqmtxu1x)p83usp7k5@wdxi(p!%d-n+2u*yh7r*mvmd8p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['resume-environment.eba-4jgqcg42.us-west-2.elasticbeanstalk.com','127.0.0.1']
+ALLOWED_HOSTS = ['project-resume-env.eba-xt3feqtk.us-west-2.elasticbeanstalk.com','127.0.0.1']
 
 
 # Application definition
